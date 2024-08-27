8bc9a97e7c74fb08adab7b60032450ffe52cb4cf
Asher Jordan <TheAsherBot@gmail.com>
2024-08-27 09:59:54 -0500

    Trying to get the Asset Path to save preferences
    I am working on finding the current Asset path, so I can save the preferences there, so the user can have the same preferences on all unity projects.

--- Unity Time Manager/Assets/NewBehaviourScript.cs
+++ Unity Time Manager/Assets/NewBehaviourScript.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+
+using Unity.VisualScripting;
 
 using UnityEditor;
 
@@ -40,32 +43,84 @@ public class NewBehaviourScript : MonoBehaviour
         // Get the type of the PackageManagerPrefs class
         Assembly assembly = Assembly.GetAssembly(typeof(Editor));
 
-        
+
 
         Debug.Log("assembly : " + assembly);
         Type packageManagerPrefsType = assembly.GetType("UnityEditor.PackageManager.UI.Internal.PackageManagerUserSettingsProvider");
 
         Debug.Log("packageManagerPrefsType: " + packageManagerPrefsType);
 
+        // object packageManagerPrefs = Activator.CreateInstance(packageManagerPrefsType, true);
+        // object packageManagerPrefs = assembly.CreateInstance(packageManagerPrefsType.FullName);
 
-        Utills.ReflectType(packageManagerPrefsType, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        // Utills.ReflectType(packageManagerPrefsType, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
 
         /////////////////////////////////
 
-        Debug.Log("providerInstance: " + null);
-        
-        PropertyInfo propertyInfo = 
-            packageManagerPrefsType
-            .GetProperty("currentAssetStoreNormalizedPath");
+        Test();
 
-        Debug.Log("propertyInfo: " + propertyInfo);
+        // Debug.Log("propertyInfo: " + propertyInfo);
 
-        object _o = propertyInfo.GetValue(null);
+        // object _o = propertyInfo.GetValue(null);
 
 
         // Utills.ReflectType(packageManagerPrefsType, BindingFlags.CreateInstance | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.GetProperty | BindingFlags.SetProperty | BindingFlags.GetField | BindingFlags.SetField | BindingFlags.CreateInstance | BindingFlags.InvokeMethod);
 
+        void Test()
+        {
+            Debug.Log("providerInstance: " + null);
 
+            ConstructorInfo[] constructorInfo = packageManagerPrefsType.GetConstructors(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            for (int i = 0; i < constructorInfo.Length; i++)
+            {
+                string parameterString = "";
+                ParameterInfo[] parameterInfo = constructorInfo[i].GetParameters();
+                for (int j = 0; j < parameterInfo.Length; j++)
+                {
+                    parameterString += $"\nName: {parameterInfo[j].Name}, Type: {parameterInfo[j].ParameterType}, Defualt: {parameterInfo[j].DefaultValue}, isOut: {parameterInfo[j].IsOut}";
+                }
+                
+                Debug.Log($"Constructor {i}\nName: {constructorInfo[i].Name}\nisPublic: {constructorInfo[i].IsPublic}\nIsPrivate: {constructorInfo[i].IsPrivate}\nIsStatic: {constructorInfo[i].IsStatic}\nIsStatic: {constructorInfo[i]}{parameterString}");
+
+            }
+
+
+            // object packageManagerPrefs = constructorInfo[0].Invoke(new object[] { "Hello WOrld!", new string[] { } });
+            // Debug.Log("providerInstance: " + packageManagerPrefs);
+
+            FieldInfo[] fieldInfo = packageManagerPrefsType.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            for (int i = 0; i < fieldInfo.Length; i++)
+            {
+                Debug.Log($"field {i}\nName: {fieldInfo[i].Name}\nReturn: {fieldInfo[i].FieldType}\nisPublic: {fieldInfo[i].IsPublic}\nIsPrivate: {fieldInfo[i].IsPrivate}\nIsStatic: {fieldInfo[i].IsStatic}\nValue {fieldInfo[i].GetValue(null)}");
+            }
+
+            PropertyInfo[] propertyInfo = packageManagerPrefsType.GetProperties(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            for (int i = 0; i < propertyInfo.Length; i++)
+            {
+                Debug.Log($"field {i}\nName: {propertyInfo[i].Name}\nReturn: {propertyInfo[i].PropertyType}\nIsStatic: {fieldInfo[i].IsStatic}\nValue {fieldInfo[i].GetValue(null)}");
+            }
+
+            MethodInfo[] methodInfo = packageManagerPrefsType.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            for (int i = 0; i < methodInfo.Length; i++)
+            {
+               ;
+
+
+                string parameterString = "";
+                ParameterInfo[] parameterInfo = methodInfo[i].GetParameters();
+                for (int j = 0; j < parameterInfo.Length; j++)
+                {
+                    parameterString += $"\nName: {parameterInfo[j].Name}, Type: {parameterInfo[j].ParameterType}, Defualt: {parameterInfo[j].DefaultValue}, isOut: {parameterInfo[j].IsOut}";
+                }
+
+                Debug.Log($"Method {i}\nName: {methodInfo[i].Name}\nReturn: {methodInfo[i].ReturnType}\nisPublic: {methodInfo[i].IsPublic}\nIsPrivate: {methodInfo[i].IsPrivate}\nIsStatic: {methodInfo[i].IsStatic}{parameterString}\n Has Atribute: {methodInfo[i].GetCustomAttributes(typeof(SettingsProvider), false).Length}");
+            }
+            // Debug.Log(methodInfo[0].Invoke(packageManagerPrefs, new object[] { }));
+        }
 
 
         if (packageManagerPrefsType != null)
